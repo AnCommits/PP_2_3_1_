0b44a522fcb0e0f7461869c24a8c5490f14055de
AnUser99 <kisamasyanya@gmail.com>
2023-11-04 16:32:39 +0300

    String addUser()

--- src/main/java/ru/andrey/controller/UserController.java
+++ src/main/java/ru/andrey/controller/UserController.java
@@ -3,12 +3,10 @@ package ru.andrey.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.andrey.model.User;
 import ru.andrey.service.UserService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,6 +18,14 @@ public class UserController {
         this.userService = userService;
     }
 
+    // Удаление всех записей из таблицы и добавление 4 пользователей
+    @GetMapping("/init")
+    public String init() {
+        userService.initTable();
+        return "redirect:users";
+    }
+
+    // Отображение всех пользователей
     @GetMapping("/users")
     public String showUsers(ModelMap model) {
         List<User> users = userService.getAllUsers();
@@ -27,15 +33,21 @@ public class UserController {
         return "users";
     }
 
-    @GetMapping("/add_user")
-    public String showAddUser(ModelMap model) {
-        model.addAttribute("user", new User());
-        return "add_user";
-    }
+//    @GetMapping("/add_user")
+//    public String showAddUser(ModelMap model) {
+//        model.addAttribute("user", new User());
+//        return "add_user";
+//    }
 
-    @PostMapping("/addus")
-    public String addUser(@ModelAttribute User user, ModelMap model) {
+    @PostMapping("/add_user")
+    public String addUser() {
         System.out.println("Method addUser()");
         return "redirect:users";
     }
+
+//    @PostMapping("/addus")
+//    public String addUser(@ModelAttribute User user, ModelMap model) {
+//        System.out.println("Method addUser()");
+//        return "redirect:users";
+//    }
 }
